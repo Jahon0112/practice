e638ae0e93c6e40aabc284483e0bbea6624134e5
Jakhongir <jumaevjakhongir6801@gmail.com>
2026-09-16 12:45:46 +0900

    fix: python special/megic methods

--- class.about .py
+++ class.about .py
@@ -27,10 +27,9 @@ class Person ():
     def say_age(self):
         print(f"{self.name}says I am{self.age}!")
 
-
-@classmethod
-def explain(cls):
-    print("static method property executed!")
+    @classmethod
+    def explain(cls):
+        print("static method property executed!")
 
 
 person1 = Person("Justin", 25)
@@ -40,7 +39,7 @@ person3 = Person("John", 22)
 # ordinary state property
 print("person1.name:", person1.name)
 
-# ordinary method
+# ordinary +
 person1.introduce()
 person2.say_age()
 
@@ -51,3 +50,47 @@ print("new_message:", new_message)
 
 # static method
 Person.explain()
+
+
+print("====special/megic methods====")
+# Python's most common special methods are below
+# __init __ __new__ __str__ __call__ __getitem__ __eq__ __len__...
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+        # method
+    def start_engine(self):
+        print(f"the{self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"the {self.name} was produced in {self.year}year!"
+
+    def __call__(self):
+        print("Object called as function")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("------")
+your_car = Car("Toyota", 2026)
+print(your_car)
+response = your_car()
+print("response:", response)
